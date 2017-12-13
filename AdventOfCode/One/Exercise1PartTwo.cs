53fbe16a7d9b734030d442640bd8cd946aaebd41
MOONPIG\jonathan.bock <jonathan.bock@moonpig.com>
2017-12-13 11:47:16 +0000

    Added part two

---
+++ AdventOfCode/One/Exercise1PartTwo.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.One
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    public class Exercise1PartTwo
+    {
+        public int SutPartTwo(string input)
+        {
+            var intputList = input.ToCharArray().ToList();
+            var halfwayCount = intputList.Count / 2;
+
+            var calculationList = new List<int>();
+            for (int i = 0; i < intputList.ToList().Count; i++)
+            {
+                
+            }
+
+           return 0;
+        }
+
+        [Fact]
+        public void ExampleOne_GetSix()
+        {
+            var input = "1212";
+            var result = SutPartTwo(input);
+
+            result.Should().Be(6);
+        }
+    }
+}
---
+++ AdventOfCode/One/SutPartOne.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.One
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SutPartOne
+    {
+        public int Handle(string input)
+        {
+            var intputList = input.ToCharArray().ToList();
+
+            var previousNum = 0;
+            var firstNumber = 0;
+            var calculationList = new List<int>();
+            for (int i = 0; i < intputList.ToList().Count; i++)
+            {
+                var currentNumber = Convert.ToInt32(intputList[i].ToString());
+                if (i == 0)
+                {
+                    previousNum = currentNumber;
+                    firstNumber = currentNumber;
+                    continue;
+                }
+
+                if (currentNumber == previousNum)
+                {
+                    calculationList.Add(currentNumber);
+                }
+                previousNum = currentNumber;
+            }
+
+            if (firstNumber == previousNum)
+            {
+                calculationList.Add(firstNumber);
+            }
+
+            return calculationList.Sum(x => Convert.ToInt32(x));
+        }
+    }
+}
